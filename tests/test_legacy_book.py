242b20131490660fc0d38973cbb72ae140488062
martialartistslife <317283960+martialartistslife@users.noreply.github.com>
2026-08-30 04:24:44 -0600

    Test legacy barcode mapping normalization

--- tests/test_legacy_book.py
+++ tests/test_legacy_book.py
@@ -45,8 +45,14 @@ def test_zero_padded_ean13_scanner_representation_is_accepted():
     )
 
 
+def test_zero_padded_scanner_form_shares_the_same_mapping_key():
+    assert legacy_book.mapping_key(KRISTY_UPC5) == KRISTY_UPC5
+    assert legacy_book.mapping_key("007807300350143506") == KRISTY_UPC5
+
+
 def test_arbitrary_ean13_plus_supplement_is_not_reinterpreted_as_upca():
     assert legacy_book.parse("107807300350143506") is None
+    assert legacy_book.mapping_key("107807300350143506") is None
 
 
 def test_invalid_upc_check_digit_is_not_treated_as_legacy_book():
